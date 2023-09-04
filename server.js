4bacc6c06235a457768067cae3f063dd98484217
Luis Alfredo <alf.182.u@gmail.com>
2023-09-03 21:45:43 -0400

    Adicionando axios

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require("express");
-const app = express();
+const axios = require("axios");
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+const app = express();
+const url = "http://localhost:3000/property";
+
+app.get("/properties", function (req, res) {
+  // Realizar la llamada HTTP utilizando Axios
+  axios
+    .get(`${url}/`)
+    .then((response) => {
+      // Manejar la respuesta del servidor remoto
+      res.send(response.data); // Enviar la respuesta al cliente de Express
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Error en la llamada HTTP");
+    });
 });
 
 app.listen(3002);
